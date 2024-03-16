16a5f04b3d7cc9ea9535bac6595f0a1024eac97a
atto08 <atto08@naver.com>
2024-03-16 15:23:33 +0900

    initial commit

---
+++ src/test/java/com/project/MobiliUs/MobiliUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.MobiliUs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobiliUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
